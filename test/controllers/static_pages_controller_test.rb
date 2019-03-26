b0d3e22e2f5364378d84152a01351fe816ef1755
Noah <nbtheduke@gmail.com>
2019-03-26 19:39:12 -0400

    Add more static pages and some tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,23 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "NetrunnerDB | Android: Netrunner Cards and Deckbuilder"
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+    assert_select "title", "NetrunnerDB | Help"
+  end
+
+  test "should get cards" do
+    get static_pages_cards_url
+    assert_response :success
+    assert_select "title", "NetrunnerDB | Cards"
+  end
+
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
