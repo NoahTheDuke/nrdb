2da698bb015fc04b7876a28a9a4d9e7f8507b65d
Noah <nbtheduke@gmail.com>
2019-03-27 10:34:15 -0400

    DRY page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "NetrunnerDB | Android: Netrunner Cards and Deckbuilder"
+    assert_select "title", "Android: Netrunner Cards and Deckbuilder"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "NetrunnerDB | Help"
+    assert_select "title", "Help | NetrunnerDB"
   end
 
   test "should get cards" do
     get static_pages_cards_url
     assert_response :success
-    assert_select "title", "NetrunnerDB | Cards"
+    assert_select "title", "Cards | NetrunnerDB"
   end
 
   test "should get root" do
