1c64dad0192eaeac9fc37e05f07e21fcddbd0faa
Noah <nbtheduke@gmail.com>
2019-03-27 17:42:21 -0400

    Add tighter validation to user username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username, presence: true,
+                       length: { minimum: 3, maximum: 20 },
+                       uniqueness: { case_sensitive: false }
+
+  VALID_EMAIL_REGEX = /.@./
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,20 +1,55 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  describe 'A user' do
-    it 'should be valid' do
-      user = build(:user)
-      assert user.valid?
-    end
+  it 'should be valid' do
+    user = build(:user)
+    assert user.valid?
+  end
 
-    it 'with no username should be invalid' do
+  describe 'usernames' do
+    it 'require non-blank usernames' do
       user = build(:user, username: '')
       assert_not user.valid?
     end
 
-    it 'with no email should be invalid' do
+    it 'limit username length' do
+      user = build(:user, username: 'a' * 21)
+      assert_not user.valid?
+
+      user = build(:user, username: 'aa')
+      assert_not user.valid?
+    end
+
+    it 'should be unique' do
+      create(:user, username: 'noah', email: 'a@a')
+      duplicate_username = build(:user, username: 'noah', email: 'b@b')
+      assert_not duplicate_username.valid?
+    end
+  end
+
+  describe 'emails' do
+    it 'require non-blank email addresses' do
       user = build(:user, email: '')
       assert_not user.valid?
+    end
+
+    it 'limit email length' do
+      user = build(:user, email: 'a@' + 'a' * 254)
+      assert_not user.valid?
+    end
+
+    it 'require email to have text on both sides of @' do
+      invalid_addresses = %w[foo@ @bar foobar]
+      invalid_addresses.each do |invalid_address|
+        user = build(:user, email: invalid_address)
+        assert_not user.valid?, "#{invalid_address.inspect} should be invalid"
+      end
+    end
+
+    it 'should be unique' do
+      create(:user, username: 'noah', email: 'a@a')
+      duplicate_email = build(:user, username: 'bogart', email: 'a@a')
+      assert_not duplicate_email.valid?
     end
   end
 end
